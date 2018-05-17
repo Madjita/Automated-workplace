c7b84ad71769d96fed0ffbd17234c2995c709a1b
Madjita <xok-s@yandex.ru>
2018-05-17 19:31:39 +0700

    add Open Device
    add Open Device

--- automatedWorkplace/home/urls.py
+++ automatedWorkplace/home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'connect/$',views.connect, name='connect'),
+    url(r'disconnect/$',views.disconnect, name='disconnect')
 ]
--- automatedWorkplace/home/views.py
+++ automatedWorkplace/home/views.py
@@ -10,17 +10,26 @@ import sys, os , json , codecs
 from ctypes import *
 from socket import *
 
+#import visa
+#visa.log_to_screen()
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 visa64 = WinDLL( BASE_DIR + "/Visa/MiVISA32.dll")
 
-myDll = WinDLL( BASE_DIR + "/Visa/DeployDll.dll")
+rm_session = c_uint32()
+session = c_uint32()
+ViStatus = visa64.viOpenDefaultRM(byref(rm_session))
+
+
+#myDll = WinDLL( BASE_DIR + "/Visa/DeployDll.dll")
 
 
 
 def findDevice():
-    rm_session = c_uint32()
-    ViStatus = visa64.viOpenDefaultRM(byref(rm_session))
+
+
 
     expr = b"TCPIP?*"
     instr_list = c_uint32()
@@ -28,7 +37,7 @@ def findDevice():
 
     list  = []
 
-    listDevice = create_unicode_buffer(512)
+    listDevice = create_unicode_buffer(256)
 
     ViStatus = visa64.viFindRsrc(rm_session,
                             expr,
@@ -41,6 +50,8 @@ def findDevice():
 
     findStr = str(listDevice.value.encode('utf16'),'cp1251').split('\t')
     findStr[0] = findStr[0][2:-1]
+    findStr[2] = findStr[2].split(' ')
+    findStr[3] = findStr[0][2:-1].split('::')[1]
     list.append(findStr)
 
 
@@ -48,51 +59,76 @@ def findDevice():
     dontFind = None
 
     if countDevice.value == 0:
-        return JsonResponse({'ViStatus' : str(ViStatus) ,'countDevice': str(countDevice.value) ,'listDevice' : dontFind})
+        visa64.viClose(instr_list)
+        listDevice = JsonResponse({'ViStatus' : str(ViStatus) ,'countDevice': str(countDevice.value) ,'listDevice' : dontFind})
+        json_object = json.loads(listDevice.getvalue().decode('utf-8'))
+        return json_object;
     else:
         for countDevices in range(countDevice.value-1):
             ViStatus = visa64.viFindNext(instr_list,listDevice)
+            print(findStr)
             #list.append(str(listDevice.value)[2:-1])
             findStr = str(listDevice.value.encode('utf16'),'cp1251').split('\t')
             findStr[0] = findStr[0][2:-1]
+            findStr[2] = findStr[2].split(' ')
+            findStr[3] = findStr[0][2:-1].split('::')[1]
             list.append(findStr)
 
-
-
-
+    visa64.viClose(instr_list)
     #return JsonResponse({'desc' : str(desc.value)})
-    return JsonResponse({'ViStatus' : str(ViStatus) ,'countDevice': str(countDevice.value) ,'listDevice' : list})
+    listDevice =  JsonResponse({'ViStatus' : str(ViStatus) ,'countDevice': str(countDevice.value) ,'listDevice' : list})
+    json_object = json.loads(listDevice.getvalue().decode('utf-8'))
+    print(json_object['listDevice'][0][0])
+    return json_object;
 
 
 # Create your views here.
 def index(request):
     automatedWorkstation = TableAutomatedWorkstation.objects.all()
+    json_object = findDevice();
 
-    listDevice = findDevice();
+    # name = json_object['listDevice'][0][0];
+    # session = c_uint32()
+    #
+    # viStatus = visa64.viOpen(rm_session,name.encode('utf-8'),None,None,byref(session))
+    # print(viStatus,name)
 
 
-
-    listDevice = JsonResponse({'ViStatus': '0', 'countDevice': '1', 'listDevice': [['TCPIP0::10.12.1.100::8888::SOCKET', '10.12.1.66', 'Г7М-20 10008020', '10.12.1.100', '']]})
-
-
-
+    # instr_list = c_uint32()
+    # countDevice = c_uint32()
+    # listDevice = create_unicode_buffer(512)
+    #rm = visa.ResourceManager()
+    #print(rm.list_resources('TCPIP?*'))
+    #listDevice = JsonResponse({'ViStatus': '0', 'countDevice': '1', 'listDevice': [['TCPIP0::10.12.1.100::8888::SOCKET', '10.12.1.66', 'Г7М-20 10008020', '10.12.1.100', '']]})
     #print(str(listDevice.getvalue().decode('utf-8')))
-
     #return listDevice
 
-    json_object = json.loads(listDevice.getvalue().decode('utf-8'))
-
-
-
-    print(json_object['listDevice'])
-
-
-    print(myDll.DeployDll())
-    print(myDll.showMessage())
-
-
+    #print(myDll.DeployDll())
+    #print(myDll.showMessage())
     #{'ViStatus': '0', 'countDevice': '1', 'listDevice': [['TCPIP0::10.12.1.100::8888::SOCKET', '10.12.1.66', 'Г7М-20 10008020', '10.12.1.100', '']]}
 
 
 
+
     return render(request, 'home/index.html', {'automatedWorkstation': automatedWorkstation, 'listDevice' : json_object })
+
+def connect(request):
+    if request.method == 'GET':
+        print("LOLKA",  request.GET['name'])
+        name = request.GET['name']
+        viStatus = visa64.viOpen(rm_session,name.encode('utf-8'),None,None,byref(session))
+        print(viStatus,name)
+    return HttpResponse(request)
+
+def disconnect(request):
+    if request.method == 'GET':
+        print("LOLKA disconnect",  request.GET['name'])
+        name = request.GET['name']
+        print(name)
+        visa64.viClose(session);
+    return HttpResponse(request)
+
+
+    #
+    # YOUR_OBJECT.objects.filter(name=name).update(view=F('views')+1)
+    # return HttpResponseRedirect(request.Get.get('next'))
